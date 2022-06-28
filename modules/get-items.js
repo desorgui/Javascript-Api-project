f4b3a13682b1300c0dbaa0d2da533d4d49b79e77
Ibnballo1 <belloabdullateef035@gmail.com>
2022-06-28 23:30:38 +0100

    Update get-items

--- modules/get-items.js
+++ modules/get-items.js
@@ -1,16 +1,15 @@
 // Fetch item from API
 
-import { rest } from "lodash";
-
+import displayList from "./display_list";
 const getData = async () => {
     const url = 'https://www.themealdb.com/api/json/v1/1/categories.php';
     const request = new Request(url);
     const results = await fetch(request);
     const gameObj = await results.json();
-    console.log(gameObj.categories);
+    //console.log(gameObj.categories);
     const [ ...result ] = gameObj.categories;
-    console.log(result);
-    //displayGames(result);
+    //console.log(result);
+    displayList(result);
   };
   
   export default getData;

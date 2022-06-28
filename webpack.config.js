63f21bd794e334842ee9fb305fa655fcfbafd1e1
Ibnballo1 <belloabdullateef035@gmail.com>
2022-06-28 10:04:46 +0100

    Fix image issue

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
             loader: 'file-loader',
             options: {
               name: '[name].[ext]',
-              outputPath: 'assets/images/'
+              outputPath: 'images/'
             }
           }
         ]

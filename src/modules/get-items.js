add6073e7186986712d7040d14faca35b5e318b4
Ibnballo1 <belloabdullateef035@gmail.com>
2022-06-29 20:36:45 +0100

    Update get-items

--- src/modules/get-items.js
+++ src/modules/get-items.js
@@ -1,5 +1,6 @@
 // Fetch item from API
 import displayList from './display_list.js';
+import getLikes from './getlikes.js';
 
 const getData = async () => {
   const url = 'https://www.themealdb.com/api/json/v1/1/categories.php';
@@ -7,6 +8,14 @@ const getData = async () => {
   const results = await fetch(request);
   const mealObj = await results.json();
   const result = mealObj.categories;
+  const likesObj = getLikes();
+
+  const combinedAPI = result + likesObj;
+  /*if (results.status === 200) {
+    const combinedAPI = {...result, ...likesObj};
+    console.log(combinedAPI);
+  }*/
+  console.log(combinedAPI);
   displayList(result);
   return result;
 };
